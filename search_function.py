819d0012f403beaae92ccfcd0cfb4f332b3f49a5
MithunW <mithun.17@cse.mrt.ac.lk>
2021-11-25 07:58:13 +0530

    changed new

--- search_function.py
+++ search_function.py
@@ -6,17 +6,13 @@ client = Elasticsearch(HOST="http://localhost",PORT=9200)
 # INDEX = 'srilankan_cricketers_tokenized'
 INDEX =  'lka-cricketer-index'
 
-# sinhala_popularity=['හොඳම','ජනප්‍රිය','ප්‍රචලිත','ප්‍රසිද්ධ','හොදම','ජනප්‍රියම']
-# english_popularity=['best','famous','top','most famous','toopest']
+synbirth_place = ['තැන','ස්ථානය','ප්‍රදේශය', 'උපන්ගම', 'ගම','ප්‍රාන්තය', 'පළාත','රට']
+synbirth_date = ['උපන්දිනය','ජන්ම දිනය','උපන්','උපතලද','උපත','මෙලොවට ආ','මෙලොව එළිය දුන්','එළිය දුටු','මෙලොවට','ඉපදුන', 'වන දින']
+synFemale = ["ගැහැනු", "ගැහැණු", "ස්ත්‍රී", "ක්‍රීඩිකා", "ක්‍රීඩිකාවන්", "ක්‍රීඩිකාව", "කාන්තාව", "කාන්තාවන්", "කාන්තා"]
+synMale = ["පුරුශ","පිරිමි","ක්‍රීඩකයන්","ක්‍රීඩකයා","ක්‍රීඩකයෝ"]
+synDead = ["අභාවප්‍රාප්ත", "මළ", "නැසී ගිය", "නැසිගිය", "මැරුනු", "මියගිය"]
 
-# synonym_artist = ['ගායකයා','ගයනවා','ගායනා','ගායනා','ගැයු','ගයන','ගයපු']
-# synonym_eng_artist = ['sing', 'artist','singer','sung','sang']
-# synonym_lyrics = ['ගත්කරු','රචකයා','ලියන්නා','ලියන','රචිත','ලියපු','ලියව්‌ව','රචනා','රචක','ලියන්','ලියූ']
-# synonym_eng_lyrics = ['lyricist','write','wrote','writer','songwriter','written']
-# synonym_music = ['සංගීත','සංගීතවත්','සංගීතය']
-# synonym_eng_music = ['composer','music','composed','compose','musician']
-
-# synonym_list = [ synonym_eng_artist, synonym_artist, synonym_eng_lyrics, synonym_lyrics, synonym_eng_music, synonym_music]
+synonym_list = [ synbirth_place, synbirth_date, synDead]
 
 
 
@@ -26,54 +22,61 @@ def search(search_query):
     processed_tokens = search_query.split()
     search_fields = []
     sort_num = 0
-    field_list = ["cricketerName","paragraph", "age",]
+    field_list = ["Birth","Birth","Death"]
     all_fields = ["cricketerName","bio","paragraph", "Gender", "Is", "Birth", "Age", "Star sign","Sports Teams","Death","Profiles"]
     final_fields = []
 
     for word in tokens:
         print (word)
 
-        # if (word in sinhala_popularity) or (word in english_popularity):
-        #     processed_tokens.remove(word)
-        #     print('Start sort by views')
-        #     sort_num = 986
+        if (word in synMale and not("පිරිමි" in word)):
+            processed_tokens.remove(word)
+            processed_tokens.append("පිරිමි")
+        elif(word in synMale and not("පිරිමි" in word)):
+            processed_tokens.remove(word)
+            search_fields.append("Gender")
+        if (word in synFemale and not("ගැහැණු" in word)):
+            processed_tokens.remove(word)
+            processed_tokens.append("ගැහැණු")
+        elif(word in synFemale and not("ගැහැණු" in word)):
+            processed_tokens.remove(word)
+            search_fields.append("Gender")
 
         # if word.isdigit():
         #     sort_num = int(word)
         #     processed_tokens.remove(word)
         #     print ('Identified sort number',sort_num)
 
-        # for i in range(0, 6):
-        #     if word in synonym_list[i]:
-        #         print('Adding field', field_list[i], 'for ', word, 'search field list')
-        #         search_fields.append(field_list[i])
-        #         if(i%2==0):
-        #             search_fields.append(field_list[i+1])
-        #         else:
-        #             search_fields.append(field_list[i -1])
-        #         processed_tokens.remove(word)
+        for i in range(0, 3):
+            if word in synonym_list[i]:
+                print('Adding field', field_list[i], 'for ', word, 'search field list')
+                search_fields.append(field_list[i])
+                if(i%2==0):
+                    search_fields.append(field_list[i+1])
+                else:
+                    search_fields.append(field_list[i -1])
+                processed_tokens.remove(word)
 
     if (len(processed_tokens)==0):
         processed_query = search_query
     else:
         processed_query = " ".join(processed_tokens)
 
-    ###Boosting
-    # for field in all_fields:
-    #     if (field in search_fields):
-    #         final_fields.append(field+"^5")
-    #     else:
-    #         final_fields.append(field)
+    ##Boosting
+    for field in all_fields:
+        if (field in search_fields):
+            final_fields.append(field+"^5")
+        else:
+            final_fields.append(field)
     final_fields = search_fields
 
-    if (sort_num==0):
-        print('Faceted Query')
-        # if(len(search_fields)==0):
+    
+    if(len(search_fields)==0):
         query_es = advanced_queries.multi_match_agg_cross(processed_query, all_fields)
-        # elif (len(search_fields) == 2):
-        #     query_es = advanced_queries.multi_match_agg_phrase(processed_query, all_fields)
-        # else:
-        #     query_es = advanced_queries.multi_match_agg_cross(processed_query, final_fields)
+    elif (len(search_fields) == 2):
+        query_es = advanced_queries.multi_match_agg_phrase(processed_query, all_fields)
+    else:
+        query_es = advanced_queries.multi_match_agg_cross(processed_query, final_fields)
 
     # else:
     #     print('Range Query')
