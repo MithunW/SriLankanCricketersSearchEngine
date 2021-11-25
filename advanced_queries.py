0252acf8ed3a5e34a9072e5cfc7f36b59ca6521d
MithunW <mithun.17@cse.mrt.ac.lk>
2021-11-25 08:23:51 +0530

    commitlastest

--- advanced_queries.py
+++ advanced_queries.py
@@ -1,97 +1,5 @@
 import json
 
-#best_filds
-# def multi_match_agg_best(query, fields=['cricketerName','paragraph']):
-# 	print ("QUERY FIELDS")
-# 	print (fields)
-# 	q = {
-# 		"size": 986,
-# 		"explain": True,
-# 		"query": {
-# 			"multi_match": {
-# 				"query": query,
-# 				"fields": fields,
-# 				"operator": 'or',
-# 				"type": "best_fields"
-# 			}
-# 		},
-# 		"aggs": {
-# 			"Genre Filter": {
-# 				"terms": {
-# 					"field": "genre.keyword",
-# 					"size": 10
-# 				}
-# 			},
-# 			"Music Filter": {
-# 				"terms": {
-# 					"field": "music.keyword",
-# 					"size": 10
-# 				}
-# 			},
-# 			"Artist Filter": {
-# 				"terms": {
-# 					"field": "artist.keyword",
-# 					"size": 10
-# 				}
-# 			},
-# 			"Lyrics Filter": {
-# 				"terms": {
-# 					"field": "lyrics.keyword",
-# 					"size": 10
-# 				}
-# 			}
-# 		}
-# 	}
-
-# 	q = json.dumps(q)
-# 	return q
-
-
-# def multi_match_agg_sort_best(query, sort_num, fields=['title','song_lyrics']):
-# 	print ('sort num is ',sort_num)
-# 	q = {
-# 		"size": sort_num,
-# 		"sort": [
-# 			{"views": {"order": "desc"}},
-# 		],
-# 		"query": {
-# 			"multi_match": {
-# 				"query": query,
-# 				"fields": fields,
-# 				"operator": 'or',
-# 				"type": "best_fields"
-# 			}
-# 		},
-# 		"aggs": {
-# 			"Genre Filter": {
-# 				"terms": {
-# 					"field": "genre.keyword",
-# 					"size": 10
-# 				}
-# 			},
-# 			"Music Filter": {
-# 				"terms": {
-# 					"field": "music.keyword",
-# 					"size": 10
-# 				}
-# 			},
-# 			"Artist Filter": {
-# 				"terms": {
-# 					"field": "artist.keyword",
-# 					"size": 10
-# 				}
-# 			},
-# 			"Lyrics Filter": {
-# 				"terms": {
-# 					"field": "lyrics.keyword",
-# 					"size": 10
-# 				}
-# 			}
-# 		}
-# 	}
-# 	q = json.dumps(q)
-# 	return q
-
 #cross_filds
 def multi_match_agg_cross(query, fields=['"cricketerName"',"paragraph"]):
 	print ("QUERY FIELDS")
@@ -113,26 +21,6 @@ def multi_match_agg_cross(query, fields=['"cricketerName"',"paragraph"]):
 	return q
 
 
-# def multi_match_agg_sort_cross(query, sort_num, fields=['title','song_lyrics']):
-# 	print ('sort num is ',sort_num)
-# 	q = {
-# 		"size": sort_num,
-# 		"sort": [
-# 			{"views": {"order": "desc"}},
-# 		],
-# 		"query": {
-# 			"multi_match": {
-# 				"query": query,
-# 				"fields": fields,
-# 				"operator": 'or',
-# 				"type": "cross_fields"
-# 			}
-# 		},
-# 	}
-# 	q = json.dumps(q)
-# 	return q
-
-
 #phrase_filds
 def multi_match_agg_phrase(query, fields=['"cricketerName"',"paragraph"]):
 	print ("QUERY FIELDS")
@@ -152,24 +40,3 @@ def multi_match_agg_phrase(query, fields=['"cricketerName"',"paragraph"]):
 
 	q = json.dumps(q)
 	return q
-
-
-# def multi_match_agg_sort_phrase(query, sort_num, fields=['title','song_lyrics']):
-# 	print ('sort num is ',sort_num)
-# 	q = {
-# 		"size": sort_num,
-# 		"sort": [
-# 			{"views": {"order": "desc"}},
-# 		],
-# 		"query": {
-# 			"multi_match": {
-# 				"query": query,
-# 				"fields": fields,
-# 				"operator": 'or',
-# 				"type": "cross_fields"
-# 			}
-# 		},
-# 	}
-# 	q = json.dumps(q)
-# 	return q
-
